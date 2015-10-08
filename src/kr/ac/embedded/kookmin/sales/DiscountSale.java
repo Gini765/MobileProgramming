0b084341533fa917fb080d93b3808f528c7f8c79
GiniP <gini0723@gmail.com>
2015-10-08 19:30:01 +0900

    Lab 5 upload and Lab 6 start

---
+++ src/kr/ac/embedded/kookmin/sales/DiscountSale.java
@@ -0,0 +1,70 @@
+package kr.ac.embedded.kookmin.sales;
+
+/**
+ * Class for a sale of one item with discount expressed as a percent of the
+ * price, but no other adjustments. Class invariant: The price is always
+ * nonnegative; the name is a nonempty string; the discount is always
+ * nonnegative.
+ */
+
+public class DiscountSale extends Sale {
+	private double	discount;	// A percent of the price. Cannot be negative.
+								
+	public DiscountSale() {
+		super("no name yet", 0);
+		//this.name = "no name yet";
+		//price = 0;
+		discount = 0;
+	}
+	
+	/**
+	 * Precondition: theName is a nonempty string; thePrice is nonnegative;
+	 * theDiscount is expressed as a percent of the price and is nonnegative.
+	 */
+	public DiscountSale(String theName, double thePrice, double theDiscount) {
+		super(theName, thePrice);
+		discount = theDiscount;
+	}
+	
+	public DiscountSale(DiscountSale originalObject) {
+		super(originalObject.getName(), originalObject.getPrice());
+		discount = originalObject.discount;
+	}
+	
+	public static void announcement() {
+		System.out.println("This is the DiscountSale class.");
+	}
+	
+	public double bill() {
+		return getPrice()/100*(100-discount);
+	}
+	
+	public double getDiscount() {
+		return discount;
+	}
+	
+	/**
+	 * Precondition: Discount is nonnegative.
+	 */
+	public void setDiscount(double newDiscount) {
+		this.discount = newDiscount;
+	}
+	
+	public String toString() {
+		return (getName() + " Price = $" + getPrice() + " Discount = " + discount + "%\n" + "   Total cost = $" + bill());
+	}
+	
+	public boolean equals(Object otherObject) {
+		if (otherObject == null)
+			return false;
+		else if (getClass() != otherObject.getClass())
+			return false;
+		else {
+			DiscountSale otherDiscountSale = (DiscountSale) otherObject;
+			return (discount == otherDiscountSale.discount);
+		}
+	}
+	public DiscountSale clone() {
+		return new DiscountSale(this);
+	}
+}
